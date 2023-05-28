e5bdb9fa8e5ccf1a68c5fba36026ea1b807b9831
djanr2 <djanr2@hotmail.com>
2023-05-27 20:37:38 -0600

    20230527 first commit to angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'djanr2-first-angular-app';
+}
